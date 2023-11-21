3559267222f2ed90b62efb7d68eb0862356abb93
artemave <artemave@gmail.com>
2023-11-21 13:23:16 +0100

    Init

---
+++ codeme.test.js
@@ -0,0 +1,105 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import assert from 'node:assert';
+import codeme from './codeme.js';
+import { exec, spawn } from 'child_process';
+import util from 'util';
+
+const execPromise = util.promisify(exec);
+const spawnPromise = util.promisify(spawn);
+const tmpPath = path.join('./tmp', 'my-temp-dir-')
+
+describe('codeme', function() {
+  beforeEach(function() {
+    if (fs.existsSync(tmpPath)) {
+      fs.rmdirSync(tmpPath, { force: true })
+    }
+    fs.mkdirSync(tmpPath, { recursive: true });
+  })
+
+  it('generates failing test', async function() {
+    const prompt = `Speaking Clock
+      Write a JavaScript function that takes the time of day in digital format (e.g. '3:10') and returns the english language equivalent, as a string. Here are some examples:
+
+      1:00
+      one o'clock
+
+      2:05
+      five past two
+
+      3:10
+      ten past three
+
+      4:15
+      quarter past four
+
+      5:20
+      twenty past five
+
+      6:25
+      twenty five past six
+
+      6:32
+      six thirty two
+
+      7:30
+      half past seven
+
+      7:35
+      twenty five to eight
+
+      8:40
+      twenty to eight
+
+      9:45
+      quarter to ten
+
+      10:50
+      ten to eleven
+
+      11:55
+      five to twelve
+
+      00:00
+      midnight
+
+      12:00
+      noon
+    `
+
+    await codeme(prompt, { cwd: tmpPath })
+
+    const { stdout } = await runNodeTestCommand(tmpPath)
+    assert.match(stdout, /fail 1/)
+  })
+})
+
+async function runNodeTestCommand(cwd) {
+  return new Promise((resolve, reject) => {
+    // Spawn the process
+    const process = spawn("node", ["--test"], { cwd });
+
+    const stdout = [];
+    const stderr = [];
+
+    // Handle stdout data
+    process.stdout.on("data", (data) => {
+      stdout.push(data);
+    });
+
+    // Handle stderr data
+    process.stderr.on("data", (data) => {
+      stderr.push(data);
+    });
+
+    // Handle error event
+    process.on("error", (error) => {
+      reject(error)
+    });
+
+    // Handle close event
+    process.on("close", (code) => {
+      resolve({ code, stdout: stdout.toString(), stderr: stderr.toString() })
+    });
+  });
+}
